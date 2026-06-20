657c8c182d93b065008f9d81258975580cce67a5
Reiterei <114533647+Reiterei@users.noreply.github.com>
2026-06-19 20:59:10 -0400

    Add files via upload
    Signed-off-by: Reiterei <114533647+Reiterei@users.noreply.github.com>

--- src/data/kauai-sets.js
+++ src/data/kauai-sets.js
@@ -1,17 +1,17 @@
-export const naluSets = [
+export const kauaiSets = [
   {
-    id: "nalu_bundle_0",
-    name: "Nalu Travel Kit",
-    series: "Nalu",
+    id: "kauai_bundle_0",
+    name: "Kauai 36",
+    series: "Kauai",
     edition: null,
     version: null,
-    count: 12,
+    count: 36,
     tipType1: "Brush",  tipType2: null,
     urls: {
-      ohuhu: "https://ohuhu.com/products/ohuhu-12-colors-mini-alcohol-markers-brush-tip-nalu-travel-coloring-kit?variant=52569426886943",
+      ohuhu: "https://ohuhu.com/products/ohuhu-kauai-36-colors-dual-tips-low-odor-washable-alcohol-art-markers?variant=51206343033119",
       michaels: null,
       walmart: null,
-      amazon: null,
+      amazon: "https://www.amazon.com/Ohuhu-Washable-Alcohol-Markers-Low-odor/dp/B0FDFH4HCJ/",
     },
     colors: ["Y06", "Y19", "Y111", "Y411", "YR110", "YR312", "YR313", "YR513", "E612", "E614", "E915", "R012", "R014", "R214", "R48", "R410", "V412", "V414", "BV37", "BV311", "B05", "B010", "B112", "BG010", "BG212", "BG314", "G36", "G313", "YG114", "YG212", "YG59", "YG611", "CG25", "BGY17", "120", "0"],
   },
