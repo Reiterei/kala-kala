be333b4d3b45a6f176daa80bfe12870ec7ee79a8
Reiterei <114533647+Reiterei@users.noreply.github.com>
2026-06-19 21:01:27 -0400

    Update all-sets.js
    Signed-off-by: Reiterei <114533647+Reiterei@users.noreply.github.com>

--- src/data/all-sets.js
+++ src/data/all-sets.js
@@ -5,7 +5,7 @@ import { naluSets } from './nalu-sets';
 import { kauaiSets } from './kauai-sets';
 
 // Add new series imports above and append below to register them everywhere.
-export const allSets = [...honoluluSets, ...kaalaSets, ...molokaiSets, ...naluSets, ...kauaiSets,];
+export const allSets = [...honoluluSets, ...kaalaSets, ...kauaiSets, ...molokaiSets, ...naluSets,];
 
 export const SERIES_ORDER = [
   'Honolulu', 'Honolulu B', 'Honolulu Plus', 'Honolulu S', 'Honolulu²', 'Honolulu² B',
